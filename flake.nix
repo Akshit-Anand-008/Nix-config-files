8107b0f02d2aa099f339ead768a97896c43ddccf
Akshit_Anand <akshitanand008@gmail.com>
2026-09-07 19:02:20 +0530

    Commit on 2026-09-07 19:02

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My first flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url ="github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager= {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
